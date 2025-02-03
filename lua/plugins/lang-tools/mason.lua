3681a9e41bd916bf0c2df098aaa8e29e9647c5ad
rodriguezjasonlloyd <rodriguezjasonlloyd@gmail.com>
2025-02-04 03:40:34 +0800

    refactor(mason): add mason-tool-installer and setup configs

--- lua/plugins/lang-tools/mason.lua
+++ lua/plugins/lang-tools/mason.lua
@@ -1,4 +1,32 @@
 return {
     "williamboman/mason.nvim",
-    opts = {},
+    dependencies = {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        require("mason").setup({})
+
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "jsonls",
+                "lua_ls",
+                "luau_lsp",
+                "ts_ls",
+            },
+        })
+
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                -- Formatter
+                "prettierd",
+                "stylua",
+
+                -- Linter
+                "eslint_d",
+                "selene",
+            },
+            auto_update = true,
+        })
+    end,
 }
