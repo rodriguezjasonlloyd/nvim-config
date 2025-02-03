740c3278f12fb5a12819b72b993d9a6797d22bfa
rodriguezjasonlloyd <rodriguezjasonlloyd@gmail.com>
2025-02-04 05:04:24 +0800

    feat(bufferline): add diagnostics to bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,8 +5,15 @@ return {
     opts = {
         options = {
             indicator = { style = "none" },
-            show_buffer_icons = false,
-            show_buffer_close_icons = false,
+            diagnostics = "nvim_lsp",
+            diagnostics_indicator = function(_, _, diagnostics_dict, _)
+                local s = " "
+                for e, n in pairs(diagnostics_dict) do
+                    local sym = e == "error" and " " or (e == "warning" and " " or " ")
+                    s = s .. n .. " " .. sym
+                end
+                return s
+            end,
         },
     },
 }
